ca694413a8ddf53e9cccf066034732f56a0c9563
Joel Friis <joefri@kth.se>
2025-12-08 13:02:15 +0100

    Update root.jsx

--- src/root.jsx
+++ src/root.jsx
@@ -36,6 +36,9 @@ const Root = observer(function Root(props) {
                     <Cart model = {props.model}/>
                 </>
             )
+        },
+        {
+            
         }
     ]);
 
