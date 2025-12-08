3fc668a1f614fbb4a50673509f7bff6fd9178304
Isac Almqvist <isacol@kth.se>
2025-12-08 14:47:12 +0100

    styling of header and cart

--- src/views/CartView.jsx
+++ src/views/CartView.jsx
@@ -10,7 +10,7 @@ export const CartView = observer(function CartRender(props) {
                 grid grid-cols-[repeat(auto-fit,minmax(240px,1fr))] 
                 grid-cols-1
                 sm:grid-cols-2
-                md:grid-cols-2 
+                md:grid-cols-3
                 lg:grid-cols-3">
                 {props.cartItems.map(renderCartItemsCB)}
             </div>
@@ -27,33 +27,38 @@ export const CartView = observer(function CartRender(props) {
         return (
             <div
                 key={item.article.id} 
-                className="flex flex-row justify-between bg-white rounded-2xl shadow-md hover:shadow-xl 
+                className="flex flex-row justify-between bg-white rounded-2xl shadow-md hover:shadow-xl relative
                     hover:-translate-y-1 transition-all duration-300 overflow-hidden border border-gray-100"
             >
                 <div 
                     className="ms-2 p-3 cursor-pointer items-center flex flex-col"
                 >
-                    <p className="fs-3 font-bold text-gray-800 tracking-tight">
+                    <p className="font-bold text-md text-gray-800 mr-auto z-10 max-w-[80%] tracking-tight">
                         {formattedTitle}
                     </p>
+
                     <img
                         src={item.article.mainImgSrc}
                         alt={formattedAlt}
                         className="h-28 object-cover rounded-md -my-[20px] mx-10"
                     />
-                    <p className="fs-2 ml-auto font-bold text-gray-800 tracking-tight">
+
+                    <p className="z-10 mt-auto ml-auto font-bold text-gray-800 tracking-tight">
                         {Utils.formatStoreName(item.store)}
                     </p>
+
+                    <button
+                        className="
+                            absolute top-2 right-2
+                            p-3 px-4 bg-gray-50 hover:bg-gray-200 border-t border-gray-100
+                            flex items-center justify-center gap-2 text-red-400 
+                            rounded-xl transition-colors duration-200"
+                        onClick={() => props.onRemoveItem(item.article.id)}
+                    >
+                        X
+                    </button>
                 </div>
                 
-                <button
-                    className="p-4 bg-gray-50 hover:bg-gray-200 border-t border-gray-100
-                        w-[30%] flex items-center justify-center gap-2 text-red-400 
-                        py-3 rounded-xl transition-colors duration-200"
-                    onClick={() => props.onRemoveItem(item.article.id)}
-                >
-                    X
-                </button>
             </div> 
         )
     }
